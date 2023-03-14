bbf1000734879d2b8e3ede41b736a2fefc98aef2
Akib-01 <abdullaalakibrimon02@gmail.com>
2023-03-15 00:13:54 +0600

    Manage

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminListModule } from './admin-list/admin-list.module';
-import { ManageModule } from './manager/manage/manage.module';
 import { ManageModule } from './manage/manage.module';
-import { WorkerListModule } from './worker-list/worker-list.module';
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [AdminListModule, ManageModule, WorkerListModule],
+  imports: [AdminListModule, ManageModule],
 })
 export class AdminModule {}
